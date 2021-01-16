70d64418da48c1c42fb6ef924fe273601f6b8d58
alice <zxt61542990@yeah.net>
2021-01-16 16:53:41 +0800

    idea commit test

---
+++ src/com/review/io/iofile/Test01.java
@@ -0,0 +1,10 @@
+package com.review.io.iofile;
+
+import java.io.File;
+
+public class Test01 {
+    public static void main(String[] args) {
+        System.out.println(" 路径分隔符: "+ File.pathSeparator); // 路径分隔符: ;
+
+    }
+}
